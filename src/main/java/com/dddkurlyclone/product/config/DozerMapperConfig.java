e46bbb3a116e0b431979a3f6b0f2d99a0401f2fc
melon9751@naver.com <melon9751@naver.com>
2021-10-13 23:53:49 +0900

    DozerMapper 설정을 분리하라
    App 클래스에 정의해둔 DozerMapper는 확인하지 못하고 지나칠 우려가
    있습니다. 따라서 따로 설정 패키지를 만들어 빈을 등록합니다.

---
+++ src/main/java/com/dddkurlyclone/product/config/DozerMapperConfig.java
@@ -0,0 +1,14 @@
+package com.dddkurlyclone.product.config;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DozerMapperConfig {
+    @Bean
+    public Mapper dozerMapper() {
+        return DozerBeanMapperBuilder.buildDefault();
+    }
+}
