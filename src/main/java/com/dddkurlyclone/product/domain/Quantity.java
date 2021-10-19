db3ab2406ecc36a381977dd893bb52d3038afd52
Spes <bloomspes@gmail.com>
2021-10-19 09:10:26 +0900

    feat: 도메인과 추상클래스 사이에서 불필요한 어노테이션 제거

--- src/main/java/com/dddkurlyclone/product/domain/Quantity.java
+++ src/main/java/com/dddkurlyclone/product/domain/Quantity.java
@@ -1,15 +1,10 @@
 package com.dddkurlyclone.product.domain;
 
 import lombok.Getter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
 
 @Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class Quantity {
     @Column
     private Integer quantity;
